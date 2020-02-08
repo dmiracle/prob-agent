1592d083ef85918285f8dd7b473b4ef3410be5c5
Dylan <dylan.miracle@gmail.com>
2020-02-08 15:07:00 +0000

    more population methods

--- population.py
+++ population.py
@@ -1,11 +1,29 @@
 import agent
 
+class Population:
 
-def init_population(size, printing=False):
-    agents = []
-    for i in range(size):
-        x = agent.Agent()
-        if printing:
-            print(i, ": ", x.name, x.uid)
-        agents.append(x)
-    return agents
+    def __init__(self):
+
+    def init_population(self, size, printing=False):
+        self.population = []
+        for i in range(size):
+            x = agent.Agent()
+            if printing:
+                print(i, ": ", x.name, x.uid)
+            self.population.append(x)
+        return self.population
+
+    def iterative_runout(self, key, val):
+        inc = 0 
+        res = []
+        while self.howMany(key, val) > 0:
+            for a in population:
+                if a.props[key] == val:
+                    a.recheck_prop(key)
+            
+            res.append([inc, self.howMany(key, val)])
+            inc += 1
+        return res
+
+    def howMany(key, value):
+        return len([1 for a in self.population if a.props[key] == value ])
