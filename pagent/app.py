b8b498d416b70830afcff0f163a39cc4083be3c7
Dylan Miracle <dylan.miracle@gmail.com>
2020-02-15 23:48:23 -0600

    update where pip is executed

--- pagent/app.py
+++ pagent/app.py
@@ -1,4 +1,8 @@
-import pagent
+from pagent.agent import Agent
+from pagent.population import Population
+from pagent.distribution import Distribution
 
 def run():
-    print("Running")
+    print("Running . . .")
+    population = Population()
+    population.init_population(25, True)
