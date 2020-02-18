627189fba3dc4b6750bac950a399a0e42c116588
Dylan Miracle <dylan.miracle@gmail.com>
2020-02-18 00:19:30 +0000

    stuff to stash

--- pagent/app.py
+++ pagent/app.py
@@ -1,4 +1,10 @@
-import pagent
+from pagent import agent
 
 def run():
     print("Running")
+    agent_count = 100
+    agents = []
+    for i in range(agent_count):
+        x = agent.Agent()
+        print(i, ": ", x.name, x.uid)
+        agents.append(x)
