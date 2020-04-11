8717d7bab761bb517a835b40cc8e79e9c131414d
Dylan Miracle <dylan.miracle@gmail.com>
2020-04-11 17:17:26 +0000

    added flask

--- pagent/app.py
+++
@@ -1,9 +0,0 @@
-from pagent.agent import Agent
-from pagent.population import Population
-from pagent.distribution import Distribution
-
-def run():
-    print("Running . . .")
-    population = Population()
-    population.init_population(25, True)
-    disty = Distribution()
