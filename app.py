9b148cb34b4053b459abbc5e0891fc6c4e9a9314
Dylan Miracle <dylan.miracle@gmail.com>
2020-04-13 04:03:24 +0000

    remove bokeh for now

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from pagent.agent import Agent
 from pagent.population import Population
+
 from flask import Flask, render_template, jsonify
 from flask_restful import Resource, Api
 import jsonpickle
+
+import random
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,12 +14,6 @@ app_population = Population()
 app_population.init_population(25)
 app_agent = Agent()
 app_agent.init_props()
-
-@app.route("/<int:bars_count>/")
-def chart(bars_count):
-    if bars_count <= 0:
-        bars_count = 1
-    return render_template("chart.html", bars_count=bars_count)
 
 @app.route("/")
 def hello():
