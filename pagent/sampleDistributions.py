6f03d9501d685bb0cf72b785dc468bb848c074ef
Dylan Miracle <dylan.miracle@gmail.com>
2020-05-10 12:32:28 +0000

    more dists

--- pagent/sampleDistributions.py
+++ pagent/sampleDistributions.py
@@ -4,12 +4,12 @@ distributions = {
     'age' : ["interpolate", [(0, 1), (100, 0)]],
 }
 dndstats = {
-        'str' : ["dice", (3, 6)],
-        'dex' : ["dice", (3, 6)],
-        'con' : ["dice", (3, 6)],
-        'int' : ["dice", (3, 6)],
-        'wis' : ["dice", (3, 6)],
-        'cha' : ["dice", (3, 6)] 
+    'str' : ["dice", (3, 6)],
+    'dex' : ["dice", (3, 6)],
+    'con' : ["dice", (3, 6)],
+    'int' : ["dice", (3, 6)],
+    'wis' : ["dice", (3, 6)],
+    'cha' : ["dice", (3, 6)] 
 }
 iui = {
     'pregnant': [[.15, .85], [True, False]]
